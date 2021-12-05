d86d45e87eed95b7ec251d173e44bbdc1db675b7
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2021-12-05 18:19:38 +0100

    Day 4

---
+++ day4/part2.py
@@ -0,0 +1,75 @@
+import sys
+
+class Board:
+  def __init__(self, board_lines) -> None:
+    self.board = [[[int(n), False] for n in line.replace("\n", "").split(" ") if n != ""] for line in board_lines]
+
+  def mark(self, num):
+    for i, row in enumerate(self.board):
+      for j, (sq_num, marked) in enumerate(row):
+        if sq_num == num:
+          self.board[i][j][1] = True
+          break 
+
+  def row_won(self, row):
+    for j, (sq_num, marked) in enumerate(row):
+      if not marked:
+        return False
+    return True
+  def col_won(self, col):
+    for i in range(len(self.board)):
+      if not self.board[i][col][1]:
+        return False
+    return True
+
+  def has_won(self):
+    # iterate rows.
+    for row in self.board:
+      if self.row_won(row):
+        return True
+
+    # iterate columns.
+    for j in range(len(self.board[0])):
+      if self.col_won(j):
+        return True
+        
+    return False
+
+  def score(self, final_number):
+    s = 0
+    for row in self.board:
+      for num, marked in row:
+        if not marked:
+          s += num
+    
+    return s * final_number
+
+  def __repr__(self) -> str:
+    s = ""
+    for r in self.board:
+      for (sq_num, marked) in r:
+        markedchar = '\'' if marked else ''
+        s += f"{sq_num}{markedchar}".ljust(3) + " "
+      s += "\n"
+    return s
+
+def main():
+  sequence = [int(l) for l in sys.stdin.readline().split(",")]
+  puzzle_input = [l for l in sys.stdin.readlines()]
+
+  # read boards
+  boards = [Board(puzzle_input[i+1:i+6]) for i in range(0, len(puzzle_input), 6)]
+
+  for n in sequence:
+    for b in boards:
+      b.mark(n)
+
+      if b.has_won():
+        boards.remove(b)
+        if len(boards) == 1:
+          print(b.score(n))
+          return 
+    
+  
+if __name__ == "__main__":
+  main()
